e00399dab18ef07233b1fd4a6886e1877caef129
Piyush <piyushsalvi777@gmail.com>
2025-02-07 13:26:14 +0530

    fixed bootstrap missing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap is imported here as well
 import './index.css'; // Your custom styles if any
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
